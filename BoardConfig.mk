f6fc139ea264db01891594cbfe60a073181f2d03
Anay Wadhera <awadhera@berkeley.edu>
2020-11-15 01:18:15 -0800

    tp1803: import framework manifest for atfwd
    Change-Id: I23ac8efb81f4a8c341e7845a726b4fc2920cbdaf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
